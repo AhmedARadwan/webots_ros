020a6fdba4d2587a7a80303d203e6ccaf29bb2b6
Ahmed Radwan <ahmed.ali.radwan94@gmail.com>
2021-02-17 10:51:29 +0200

    Update webots_launcher.py

--- scripts/webots_launcher.py
+++ scripts/webots_launcher.py
@@ -30,11 +30,11 @@ options, args = optParser.parse_args()
 if 'WEBOTS_HOME' not in os.environ:
     sys.exit('WEBOTS_HOME environment variable not defined.')
 command = [os.path.join(os.environ['WEBOTS_HOME'], 'webots'), '--mode=' + options.mode, options.world]
+command.append('--no-sandbox')
 if options.noGui.lower() == 'true':
     command.append('--stdout')
     command.append('--stderr')
     command.append('--batch')
-    command.append('--no-sandbox')
     command.append('--minimize')
 
 subprocess.call(command)
